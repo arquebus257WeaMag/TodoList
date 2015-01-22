455039bd91e614ac38fb48860773dd2896b2cd0e
Brad Cantrell <brad777mx@hotmail.com>
2015-01-21 23:38:48 -0800

    finished

--- app/src/main/java/com/bradcantrell/todoapp/TodoActivity.java
+++ app/src/main/java/com/bradcantrell/todoapp/TodoActivity.java
@@ -4,16 +4,76 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class TodoActivity extends ActionBarActivity {
+    private ArrayList<String> todoItems;
+    private ArrayAdapter<String> todoAdapter;
+    private ListView lvItems;
+    private EditText etNewItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        todoAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, todoItems);
+        lvItems.setAdapter(todoAdapter);
+        setupListViewListener();
     }
 
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                todoItems.remove(pos);
+                todoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
+
+    public void onAddedItem(View v){
+        String itemText = etNewItem.getText().toString();
+        todoAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "todo.txt");
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            todoItems = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, todoItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
